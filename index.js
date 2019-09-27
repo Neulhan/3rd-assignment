8897961edcdac230d3987a56607a8279dbc338d3
neulhan <sting7675@naver.com>
2019-09-27 17:21:54 +0900

    api에서 영화 20개 정보룰 가져오고, card에 추가

--- index.js
+++ index.js
@@ -9,20 +9,38 @@ window.onload = async () => {
 				data: { movie: data }
 			}
 		} = await axios.get(
-			"https://yts.lt/api/v2/movie_details.json?movie_id=2019"
+			"https://yts.lt/api/v2/movie_details.json?movie_id=2018"
+			// "https://yts.lt/api/v2/list_movies.json?sort=seeds&limit=15"
+			// "https://yts.lt/api/v2/list_movies.json?quality=3D"
 		);
+		const movie_list = await axios.get(
+			'https://yts.lt/api/v2/list_movies.json?sort_by=download_count'
+		);
+		for (let i = 0; i < 20; i++) {
+			let a = movie_list.data.data.movies[i];
+			console.log(a);
+			let title = document.createElement("p");
+			let img = document.createElement("img");
+			let tag = document.createElement("p");
+			let desc = document.createElement("p");
+			title.innerText = a.title_long;
+			img.src = a.medium_cover_image;
+			tag.innerText = a.genres;
+			desc.innerText = a.description_full;
 
-		console.log(data);
-		const title = document.createElement("p");
-		const img = document.createElement("img");
-		const desc = document.createElement("p");
-		title.innerText = data.title_long;
-		img.src = data.medium_cover_image;
-		desc.innerText = data.description_full;
-		const card = document.createElement("p");
-		card.append(img, title, desc);
+			let card = document.createElement("div");
+			card.classList.add('card');
+			let descCard = document.createElement("div");
+			descCard.classList.add('side-card');
+			descCard.append(title, tag, desc);
+			card.append(img, descCard);
+			app.appendChild(card);
+		}
+
+
 		app.removeChild(loading);
-		app.appendChild(card);
+
+
 	};
 
 	await main();
